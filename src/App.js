1d3f7e9c25a551f2e11ac59aaf5f57c9a69407fb
Roman De Rose <romandr37@gmail.com>
2022-04-21 21:03:11 -0300

    numberRoulette.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
 import React, { useState } from "react";
 import { Wheel } from "react-custom-roulette";
-import "./App.css";
-
 import Board from "./components/Board";
-
-const data = [
-  { option: 0, style: { backgroundColor: "#318D42" } },
-  { option: 32, style: { backgroundColor: "#E31C2E" } },
-  { option: 15, style: { backgroundColor: "#000700" } },
-  { option: 19, style: { backgroundColor: "#E31C2E" } },
-  { option: 4, style: { backgroundColor: "#000700" } },
-  { option: 21, style: { backgroundColor: "#E31C2E" } },
-  { option: 2, style: { backgroundColor: "#000700" } },
-  { option: 25, style: { backgroundColor: "#E31C2E" } },
-  { option: 17, style: { backgroundColor: "#000700" } },
-  { option: 34, style: { backgroundColor: "#E31C2E" } },
-  { option: 6, style: { backgroundColor: "#000700" } },
-  { option: 27, style: { backgroundColor: "#E31C2E" } },
-  { option: 13, style: { backgroundColor: "#000700" } },
-  { option: 36, style: { backgroundColor: "#E31C2E" } },
-  { option: 11, style: { backgroundColor: "#000700" } },
-  { option: 30, style: { backgroundColor: "#E31C2E" } },
-  { option: 8, style: { backgroundColor: "#000700" } },
-  { option: 23, style: { backgroundColor: "#E31C2E" } },
-  { option: 10, style: { backgroundColor: "#000700" } },
-  { option: 5, style: { backgroundColor: "#E31C2E" } },
-  { option: 24, style: { backgroundColor: "#000700" } },
-  { option: 16, style: { backgroundColor: "#E31C2E" } },
-  { option: 33, style: { backgroundColor: "#000700" } },
-  { option: 1, style: { backgroundColor: "#E31C2E" } },
-  { option: 20, style: { backgroundColor: "#000700" } },
-  { option: 14, style: { backgroundColor: "#E31C2E" } },
-  { option: 31, style: { backgroundColor: "#000700" } },
-  { option: 9, style: { backgroundColor: "#E31C2E" } },
-  { option: 22, style: { backgroundColor: "#000700" } },
-  { option: 18, style: { backgroundColor: "#E31C2E" } },
-  { option: 29, style: { backgroundColor: "#000700" } },
-  { option: 7, style: { backgroundColor: "#E31C2E" } },
-  { option: 28, style: { backgroundColor: "#000700" } },
-  { option: 12, style: { backgroundColor: "#E31C2E" } },
-  { option: 35, style: { backgroundColor: "#000700" } },
-  { option: 3, style: { backgroundColor: "#E31C2E" } },
-  { option: 26, style: { backgroundColor: "#000700" } },
-];
+import { data } from "./utils/numbersRoulette";
+import "./App.css";
 
 function App() {
   const [mustSpin, setMustSpin] = useState(false);
@@ -116,7 +76,7 @@ function App() {
       {data[prizeNumber].option !== usuario && !mustSpin && (
         <h3 className="lose">"Perdiste. Gira de nuevo"</h3>
       )}
-      <Board></Board>
+      <Board />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,28 @@
-import './Board.scss';
+import "./Board.scss";
 import { numbers, chances } from "../utils/numbers";
 
-const [ zero, ...restNumbers ] = numbers;
+const [zero, ...restNumbers] = numbers;
 function Board() {
-    console.log(chances);
-    return ( 
-        <div id='Board'>
-            <div>
-                <span style={{background: 'green', color: 'white'}}>
-                    { zero.value }
-                </span>
-                <div>
-                    { 
-                        restNumbers.map(number => 
-                            <span style={{background: number.color, color: 'white'}}>{ number.value }</span>
-                            
-                    )}
-                </div>
-            </div>
+  console.log(chances);
+  return (
+    <div id="Board">
+      <div>
+        <span style={{ background: "green", color: "white" }}>
+          {zero.value}
+        </span>
+        <div>
+          {restNumbers.map((number) => (
+            <span
+              key={number.value}
+              style={{ background: number.color, color: "white" }}
+            >
+              {number.value}
+            </span>
+          ))}
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Board;
