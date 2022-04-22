1cd9013c074c1d8d64f00e03360d9bb564354f55
unknown <lautaro.pastor47@gmail.com>
2022-04-22 18:16:08 -0300

    Avance tablero

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,10 +2,19 @@ import "./Board.scss";
 import { numbers, chances } from "../utils/numbers";
 
 const [zero, ...restNumbers] = numbers;
+const colors = [chances[0], chances[1]];
+const parities = [chances[2], chances[3]];
+const sizes = [chances[4], chances[5]];
+const dozens = [chances[6], chances[7], chances[8]];
+const columns = [chances[9], chances[10], chances[11]];
+
+const [ pair, odd ] = parities;
+const [ minor, major ] = sizes;
 
 function Board({ selectedNumbers, setSelectedNumbers }) {
   console.log(chances);
-  console.log(restNumbers);
+  console.log(dozens);
+  console.log(colors, parities, sizes);
 
   const userBet = (number) => {
     console.log(number, number.value);
@@ -24,15 +33,9 @@ function Board({ selectedNumbers, setSelectedNumbers }) {
 
   return (
     <div id="Board">
-      <div>
+      <div className="container">
         <span
-          style={{
-            background: "green",
-            color: "white",
-            display: "inline-block",
-            width: "100%",
-            cursor: "pointer",
-          }}
+          className="number number--zero"
           onClick={() => userBet({ value: 0, color: "green" })}
         >
           {zero.value}
@@ -42,12 +45,34 @@ function Board({ selectedNumbers, setSelectedNumbers }) {
             <span
               className="number"
               key={number.value}
-              style={{ background: number.color, color: "white" }}
+              style={{ background: number.color }}
               onClick={() => userBet(number)}
             >
               {number.value}
             </span>
           ))}
+        </div>
+        <div className="columns">
+          {
+            columns.map(column => 
+              <span className="column" key={column.name}>{column.name} 2:1</span>
+          )}
+        </div>
+        <div className="dozens">
+          {
+            dozens.map(dozen => 
+              <span className="dozen" key={dozen.name}>{dozen.name} 2:1</span>
+          )}
+        </div>
+        <div className="double-chances">
+              <span className="chance chance__size">{ minor.name }</span>
+              <span className="chance chance__parity">{ pair.name }</span>
+              {
+                colors.map(color => 
+                  <span className="chance chance__color" key={color.name}>{ color.name }</span>
+              )}
+              <span className="chance chance__parity">{ odd.name }</span>
+              <span className="chance chance__size">{ major.name }</span>
         </div>
       </div>
     </div>
